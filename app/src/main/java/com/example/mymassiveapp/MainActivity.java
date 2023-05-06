8bc8224b3bf9b54e00b0129f9fad0450e49f855a
Suren1222121 <suromanukyan1123@gmail.com>
2023-05-06 14:14:04 +0400

    sdasf

---
+++ app/src/main/java/com/example/mymassiveapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.mymassiveapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int[] nums = new int []{1,2,3,4,5};
+
+    }
+}
